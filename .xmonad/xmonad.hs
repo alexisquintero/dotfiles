151dd55bba4b1962c1352417930f83882069fabd
Alexis Quinntero <alexis_quintero@hotmail.com.ar>
2020-08-16 14:41:09 -0300

    Styling xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,4 +4,7 @@ import XMonad.Hooks.DynamicLog
 main = xmonad =<< xmobar def
         { modMask = mod4Mask
         , terminal = "st"
+        , focusFollowsMouse = False
+        , borderWidth = 0
+        , workspaces = map show [1..12]
         }
