37d6fdcc67e0159d05a76467c34f455713f30dd1
Alexis Quinntero <alexis_quintero@hotmail.com.ar>
2020-08-18 20:39:15 -0300

    Add keymappings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,19 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad =<< xmobar def
-        { modMask = mod4Mask
-        , terminal = "st"
-        , focusFollowsMouse = False
-        , borderWidth = 0
-        , workspaces = map show [1..12]
-        }
+keyMappings = [ ((mod4Mask, xK_y), spawn "xscreensaver-command -lock")
+              , ((controlMask, xK_Print), spawn "scrot -s")
+              , ((0, xK_Print), spawn "scrot")
+              ]
+
+myConfig = defaultConfig { modMask = mod4Mask
+                        , terminal = "st"
+                        , focusFollowsMouse = False
+                        , borderWidth = 0
+                        , workspaces = map show [1..12]
+                        , logHook = dynamicLogString defaultPP >>= xmonadPropLog
+                        } `additionalKeys` keyMappings
+
+main = xmonad =<< xmobar myConfig
