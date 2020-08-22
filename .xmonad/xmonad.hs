83b91fbe076dd19a9a5fb7f1846a7747f9a67132
Alexis Quinntero <alexis_quintero@hotmail.com.ar>
2020-08-22 15:47:28 -0300

    Add more workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,25 @@
 import XMonad
 import XMonad.Actions.CycleWS(toggleWS, moveTo, Direction1D(Next, Prev), WSType(EmptyWS, NonEmptyWS))
 import XMonad.Hooks.DynamicLog
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myDmenu = "dmenu_run -nf '#F6D6BD' -nb '#20394F' -sb '#4E495F' -sf '#c3a38a'"
 
-keyMappings = [ ((mod4Mask, xK_y)              , spawn "xscreensaver-command -lock")
-              , ((controlMask, xK_Print)       , spawn "scrot -s")
-              , ((0, xK_Print)                 , spawn "scrot")
-              , ((mod4Mask, xK_d)              , spawn myDmenu)
-              , ((mod4Mask, xK_n)              , moveTo Next NonEmptyWS)
-              , ((mod4Mask, xK_p)              , moveTo Prev NonEmptyWS)
-              , ((mod4Mask, xK_q)              , toggleWS)
-              , ((mod4Mask, xK_o)              , moveTo Next EmptyWS)
-              , ((mod4Mask .|. shiftMask, xK_r), broadcastMessage ReleaseResources >> restart "xmonad" True)
+keyMappings = [ ((mod4Mask, xK_y)                   , spawn "xscreensaver-command -lock")
+              , ((controlMask, xK_Print)            , spawn "scrot -s")
+              , ((0, xK_Print)                      , spawn "scrot")
+              , ((mod4Mask, xK_d)                   , spawn myDmenu)
+              , ((mod4Mask, xK_n)                   , moveTo Next NonEmptyWS)
+              , ((mod4Mask, xK_p)                   , moveTo Prev NonEmptyWS)
+              , ((mod4Mask, xK_q)                   , toggleWS)
+              , ((mod4Mask, xK_o)                   , moveTo Next EmptyWS)
+              , ((mod4Mask .|. shiftMask, xK_r)     , broadcastMessage ReleaseResources >> restart "xmonad" True)
+              , ((mod4Mask, xK_minus)               , windows $ W.greedyView "11")
+              , ((mod4Mask .|. shiftMask, xK_minus) , windows $ W.shift "11")
+              , ((mod4Mask, xK_equal)               , windows $ W.greedyView "12")
+              , ((mod4Mask .|. shiftMask, xK_equal) , windows $ W.shift "12")
               ]
 
 myConfig = defaultConfig { modMask = mod4Mask
